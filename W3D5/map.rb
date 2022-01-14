a29a81a8e639c8ad4d1b33dee3cbcd445e67060a
SVKratzer <lukekpat@gmail.com>
2022-01-14 10:08:31 -0500

    Wrote code for init, set, and get in map class

--- W3D5/map.rb
+++ W3D5/map.rb
@@ -5,9 +5,24 @@ class Map
   end
 
   def set(key, value)
+    # Take in 2 variables 
+    # Make an array of [var1, var2]
+    # Shovel that into the map 
+
+    if @map.any?{|pair| pair[0] == key}
+      pair[1] = value if 
+    else
+      @map << [key, value] 
+    end
   end
 
   def get(key)
+    #Find out which index of map contains key at pair[0]
+    #Return pair[1]
+
+    @map.each do |pair|
+      return pair[1] if pair[0] == key
+    end
   end
 
   def delete(key)
