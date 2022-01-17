e162ced2d6deb523fe738e3475434cb5783bd904
SVKratzer <lukekpat@gmail.com>
2022-01-16 22:51:50 -0500

    Added comments

--- W4D2/skeleton/lib/mancala.rb
+++ W4D2/skeleton/lib/mancala.rb
@@ -2,32 +2,47 @@ require_relative 'board'
 require_relative 'player'
 
 class Mancala
-  def initialize(name1, name2)
+  def initialize(name1, name2) #Take in both players' names 
     @player1 = Player.new(name1, 1)
+    # Make a player instance out of player 1's name and give them Row 1 (1-6)
     @player2 = Player.new(name2, 2)
+    # Make a player instance out of player 2's name and give them Row 2 (7-12)
     @board = Board.new(name1, name2)
+    # Make a board instance with both players' names 
   end
 
   def play
     puts "Welcome to Mancala"
+    # Print welcome message 
     @board.render
+    # board.render --> render the board 
     current_player = @player1
+    # create @current_player, set it to player1
     until won?
+      #Until Loop --> Until somebody wins
       take_turn(current_player)
+      # take_turn(current_player)
       current_player = current_player == @player1 ? @player2 : @player1
+      # I don't know what this means.  I think it changes current player at the end of a turn.  
     end
 
     puts "The game is finished! #{winner_message}"
+    # When someone wins, print that someone won. 
   end
 
   def take_turn(current_player)
+    # Take in the current_player 
     move_result = :prompt
+    # set move_result to :prompt
 
     until move_result == :switch
+      #Until Loop --> Until move_result becomes :switch 
       if move_result == :prompt
+        #Check to see if move_result is :prompt 
         print_indices
+        #If it is, print_indices 
 
-        begin
+        begin 
           start_pos = current_player.prompt
           start_pos -= 1 if start_pos <= 6
           @board.valid_move?(start_pos)
--- W4D2/skeleton/lib/player.rb
+++ W4D2/skeleton/lib/player.rb
@@ -1,19 +1,27 @@
 class Player
   attr_reader :name, :side
+  # Return user's name 
+  # Return user's side of the board 
 
   def initialize(name, side)
-    @name = name
-    @side = side
+    @name = name # Take in User's name 
+    @side = side # Take in User's side of the Mancala board 
   end
 
   def prompt
     puts "#{@name}, please enter which cup you would like to start from: "
+    # Ask user for cup to start with. 
     start_pos = gets.chomp.to_i
+    # Set start_pos to the cup number they give 
     if (start_pos.between?(1,6) && @side != 1 ||
+      # check to see if starting position is between 1 & 6 AND is on the row with cups 1-6 
         start_pos.between?(7,12) && @side != 2)
+      # check to see if starting position between 7 and 12 AND is on the row with those cups
       raise "Not your side!"
+      # if it is not on their side, raise "Not your side!"
     else
       start_pos
+      # if it is on their side, return the cup number 
     end
   end
 end
