7040dfb00021df7e958a7d0bfa3e5e455c415c58
SVKratzer <lukekpat@gmail.com>
2022-01-24 15:16:11 -0500

    Adds initialize count and show methods

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -1,20 +1,26 @@
 class LRUCache
-    def initialize
+    def initialize(cache_max)
+      @cache_max = cache_max
+      @cache = []
     end
 
     def count
       # returns number of elements currently in cache
+      cache.size 
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      p cache 
     end
 
     private
+    attr_reader :cache, :cache_max
     # helper methods go here!
 
   end
